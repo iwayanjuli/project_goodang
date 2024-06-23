e7dcbfc424b5f1ac8b7aa9c36d5ac32a606c737e
wayanjuli <delapanbelas.cola@gmail.com>
2024-06-23 22:43:15 +0800

    update nuxt config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -7,14 +7,5 @@ export default defineNuxtConfig({
     //   // Konfigurasi tambahan di sini
     // },
   },
-<<<<<<< HEAD
   plugins: ["~/plugins/axios.js", "~/plugins/firebase.js"],
-=======
-  modules: ["@nuxtjs/axios"],
-  axios: {
-    baseURL: process.env.BASE_URL || "http://localhost:3000",
-    // konfigurasi axios lainnya jika diperlukan
-  },
-  plugins: ["~/plugins/firebase.js"],
->>>>>>> f267d9e87fb209d8b48e46fd3ed011c2cb6c0dc1
 });
