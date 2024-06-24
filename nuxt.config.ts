1ca292afadbbfb04fbabd77ac8862706212d34e5
wayanjuli <delapanbelas.cola@gmail.com>
2024-06-24 09:28:21 +0800

    return config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,20 +2,10 @@ import { defineNuxtConfig } from "nuxt/config";
 
 export default defineNuxtConfig({
   css: ["bootstrap/dist/css/bootstrap.min.css", "~/assets/css/main.css"],
-
-  plugins: ["~/plugins/axios.js", "~/plugins/firebase.js"],
-
   build: {
-    // Extend webpack config
-    extend(config, ctx) {
-      // Example: Add support for TypeScript
-      if (ctx.isDev && ctx.isClient) {
-        config.module.rules.push({
-          test: /\.ts$/,
-          loader: "ts-loader",
-          options: { appendTsSuffixTo: [/\.vue$/] },
-        });
-      }
-    },
+    // extend(config, ctx) {
+    //   // Konfigurasi tambahan di sini
+    // },
   },
+  plugins: ["~/plugins/axios.js", "~/plugins/firebase.js"],
 });
