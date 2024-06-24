ab0adc1a8f9ff2efbf821b99d6f5c5e03eaccc50
wayanjuli <delapanbelas.cola@gmail.com>
2024-06-24 12:35:37 +0800

    update config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,10 +2,17 @@ import { defineNuxtConfig } from "nuxt/config";
 
 export default defineNuxtConfig({
   css: ["bootstrap/dist/css/bootstrap.min.css", "~/assets/css/main.css"],
-  build: {
-    // extend(config, ctx) {
-    //   // Konfigurasi tambahan di sini
-    // },
-  },
   plugins: ["~/plugins/axios.js", "~/plugins/firebase.js"],
+  ssr: true, // Ensure SSR is enabled
+  target: "server", // Ensure SSR target is set correctly
+  publicRuntimeConfig: {
+    firebaseConfig: {
+      apiKey: "AIzaSyAM0YfcmGMeSeCbWxxfclP6BDDKAdlTxnI",
+      authDomain: "goodang-project-3f7f4.firebaseapp.com",
+      projectId: "goodang-project-3f7f4",
+      storageBucket: "goodang-project-3f7f4.appspot.com",
+      messagingSenderId: "399137880407",
+      appId: "1:399137880407:web:fcd6bc7c9e3a15358aa612",
+    },
+  },
 });
--- plugins/firebase.js
+++ plugins/firebase.js
@@ -2,29 +2,28 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 import { getAuth, signInAnonymously } from "firebase/auth";
 
-// Your web app's Firebase configuration
-const firebaseConfig = {
-  apiKey: "AIzaSyAM0YfcmGMeSeCbWxxfclP6BDDKAdlTxnI",
-  authDomain: "goodang-project-3f7f4.firebaseapp.com",
-  projectId: "goodang-project-3f7f4",
-  storageBucket: "goodang-project-3f7f4.appspot.com",
-  messagingSenderId: "399137880407",
-  appId: "1:399137880407:web:fcd6bc7c9e3a15358aa612",
-};
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const firestore = getFirestore(app);
-let auth;
-
-if (typeof window !== "undefined") {
-  auth = getAuth(app);
-  signInAnonymously(auth).catch((error) => {
-    console.error("Error signing in anonymously:", error);
-  });
-}
-
 export default defineNuxtPlugin((nuxtApp) => {
+  const config = {
+    apiKey: nuxtApp.$config.firebaseConfig.apiKey,
+    authDomain: nuxtApp.$config.firebaseConfig.authDomain,
+    projectId: nuxtApp.$config.firebaseConfig.projectId,
+    storageBucket: nuxtApp.$config.firebaseConfig.storageBucket,
+    messagingSenderId: nuxtApp.$config.firebaseConfig.messagingSenderId,
+    appId: nuxtApp.$config.firebaseConfig.appId,
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(config);
+  const firestore = getFirestore(app);
+  let auth;
+
+  if (process.client) {
+    auth = getAuth(app);
+    signInAnonymously(auth).catch((error) => {
+      console.error("Error signing in anonymously:", error);
+    });
+  }
+
   nuxtApp.provide("firebase", app);
   nuxtApp.provide("firestore", firestore);
   if (auth) {
